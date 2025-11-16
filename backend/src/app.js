8fce16049cceccea5618f8958908e19d2d58e928
Hadleymicah <Hadleyfig@gmail.com>
2025-11-16 04:24:56 +0000

    chore: making sure things are commited because remote service keeps disconnecting

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,16 +1,18 @@
-import express from 'express';
-import { postsRoutes } from './routes/posts.js';
-import bodyParser from 'body-parser';
-import cors from 'cors';
+import express from 'express'
+import { postsRoutes } from './routes/posts.js'
+import { userRoutes } from './routes/users.js'
+import bodyParser from 'body-parser'
+import cors from 'cors'
 
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
 
-postsRoutes(app);
+postsRoutes(app)
+userRoutes(app)
 
 app.get('/', (req, res) => {
-  res.send('Hello from Express!');
-});
+  res.send('Hello from Express!')
+})
 
-export { app };
+export { app }
