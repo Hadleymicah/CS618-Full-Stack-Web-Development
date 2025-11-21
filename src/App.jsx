a880833a183ea86fb05c8e7666ac1b820fcfe074
Hadleymicah <Hadleyfig@gmail.com>
2025-11-21 23:03:12 +0000

    chore: react router

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,19 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Blog } from './Blog.jsx'
 const queryClient = new QueryClient()
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Blog />,
+  },
+])
+
 export function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Blog />
+      <RouterProvider router={router} />
     </QueryClientProvider>
   )
 }
