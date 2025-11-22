97eacf1fa7bd7aeb05ab5a0f9844badf79780b7f
Hadleymicah <Hadleyfig@gmail.com>
2025-11-22 23:40:58 +0000

    chore: integreate sever side rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,20 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { Blog } from './pages/Blog.jsx'
-import { Signup } from './pages/Signup.jsx'
 import { AuthContextProvider } from './contexts/AuthContext.jsx'
-import { Login } from './pages/Login.jsx'
+import PropTypes from 'prop-types'
 
 const queryClient = new QueryClient()
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Blog />,
-  },
-  {
-    path: '/signup',
-    element: <Signup />,
-  },
-  {
-    path: '/login',
-    element: <Login />,
-  },
-])
-
-export function App() {
+export function App({ children }) {
   return (
     <QueryClientProvider client={queryClient}>
       <AuthContextProvider>
-        <RouterProvider router={router} />
+        {children}
+        Rendering React components on the server 199
       </AuthContextProvider>
     </QueryClientProvider>
   )
+}
+
+App.propTypes = {
+  children: PropTypes.element.isRequired,
 }
