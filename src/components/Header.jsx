d319cdcb51b31cc90061d5670f391a44738f595c
Hadleymicah <Hadleyfig@gmail.com>
2025-11-22 01:33:10 +0000

    chore: user ids with user names

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,17 @@
 import { Link } from 'react-router-dom'
 import { jwtDecode } from 'jwt-decode'
 import { useAuth } from '../contexts/AuthContext.jsx'
+import { User } from './User.jsx'
 
 export function Header() {
   const [token, setToken] = useAuth()
+
   if (token) {
     const { sub } = jwtDecode(token)
     return (
       <div>
         <h1>Welcome to My Blog!</h1>
-        Logged in as <b>{sub}</b>
+        Logged in as <User id={sub} />
         <br />
         <button onClick={() => setToken(null)}>Logout</button>
       </div>
