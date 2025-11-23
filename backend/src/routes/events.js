dd72ef88716c657105e648c368b18cab388fd3b9
Hadleymicah <Hadleyfig@gmail.com>
2025-11-23 16:21:28 +0000

    chore: data aggregation in backend

--- backend/src/routes/events.js
+++ backend/src/routes/events.js
@@ -1,6 +1,51 @@
-import { trackEvent } from '../services/events.js'
+import {
+  trackEvent,
+  getTotalViews,
+  getDailyViews,
+  getDailyDurations,
+} from '../services/events.js'
 import { getPostById } from '../services/posts.js'
+
 export function eventRoutes(app) {
+  app.get('/api/v1/events/totalViews/:postId', async (req, res) => {
+    try {
+      const { postId } = req.params
+      const post = await getPostById(postId)
+      if (post === null) return res.status(400).end()
+      const stats = await getTotalViews(post._id)
+      return res.json(stats)
+    } catch (err) {
+      console.error('error getting stats', err)
+      return res.status(500).end()
+    }
+  })
+
+  app.get('/api/v1/events/dailyViews/:postId', async (req, res) => {
+    try {
+      const { postId } = req.params
+      const post = await getPostById(postId)
+      if (post === null) return res.status(400).end()
+      const stats = await getDailyViews(post._id)
+      return res.json(stats)
+    } catch (err) {
+      console.error('error getting stats', err)
+      return res.status(500).end()
+    }
+  })
+
+  app.get('/api/v1/events/dailyDurations/:postId', async (req, res) => {
+    try {
+      const { postId } = req.params
+      const post = await getPostById(postId)
+      if (post === null) return res.status(400).end()
+      const stats = await getDailyDurations(post._id)
+      return res.json(stats)
+    } catch (err) {
+      console.error('error getting stats', err)
+      return res.status(500).end()
+    }
+  })
+
   app.post('/api/v1/events', async (req, res) => {
     try {
       const { postId, session, action } = req.body
